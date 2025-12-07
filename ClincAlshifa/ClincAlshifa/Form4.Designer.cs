f3a642bf171170a74bcc3b63c8ca1f258e195d93
rayamohammed <rayam4774@gmail.com>
2025-12-07 16:06:50 +0100

    form4

---
+++ ClincAlshifa/ClincAlshifa/Form4.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace ClinicAppointmentSystem
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            }
+
+            #region Windows Form Designer generated code
+
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+            {
+                this.components = new System.ComponentModel.Container();
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(800, 450);
+                this.Text = "Form4";
+
+                this.sendComment = new System.Windows.Forms.Button();
+                this.panel2 = new System.Windows.Forms.Panel();
+                this.button12 = new System.Windows.Forms.Button();
+                this.label2 = new System.Windows.Forms.Label();
+                this.main = new System.Windows.Forms.Button();
+                this.connect = new System.Windows.Forms.Button();
+                this.reserve = new System.Windows.Forms.Button();
+                this.commant = new System.Windows.Forms.Button();
+                this.about = new System.Windows.Forms.Button();
+                this.button1 = new System.Windows.Forms.Button();
+                this.label9 = new System.Windows.Forms.Label();
+                this.label1 = new System.Windows.Forms.Label();
+                this.label4 = new System.Windows.Forms.Label();
+                this.panel4 = new System.Windows.Forms.Panel();
+                this.textBox1 = new System.Windows.Forms.TextBox();
+                this.label5 = new System.Windows.Forms.Label();
+                this.comboBox1 = new System.Windows.Forms.ComboBox();
+                this.panel3 = new System.Windows.Forms.Panel();
+                this.panel1 = new System.Windows.Forms.Panel();
+                this.panel2.SuspendLayout();
+                this.panel4.SuspendLayout();
+                this.panel3.SuspendLayout();
+                this.panel1.SuspendLayout();
+                this.SuspendLayout();
+                // 
+                // sendComment
+                // 
+                this.sendComment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.sendComment.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.sendComment.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+                this.sendComment.Location = new System.Drawing.Point(369, 273);
+                this.sendComment.Name = "sendComment";
+                this.sendComment.Size = new System.Drawing.Size(97, 45);
+                this.sendComment.TabIndex = 18;
+                this.sendComment.Text = "إرسال التعليق";
+                this.sendComment.UseVisualStyleBackColor = false;
+                this.sendComment.Click += new System.EventHandler(this.sendComment_Click);
+                // 
+                // panel2
+                // 
+                this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.panel2.Controls.Add(this.button12);
+                this.panel2.Controls.Add(this.label2);
+                this.panel2.Location = new System.Drawing.Point(8, 239);
+                this.panel2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.panel2.Name = "panel2";
+                this.panel2.Size = new System.Drawing.Size(1345, 85);
+                this.panel2.TabIndex = 9;
+                // 
+                // button12
+                // 
+                this.button12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.button12.Location = new System.Drawing.Point(1656, 148);
+                this.button12.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.button12.Name = "button12";
+                this.button12.Size = new System.Drawing.Size(180, 75);
+                this.button12.TabIndex = 1;
+                this.button12.Text = "الصفحة الرئيسية";
+                this.button12.UseVisualStyleBackColor = true;
+                // 
+                // label2
+                // 
+                this.label2.AutoSize = true;
+                this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+                this.label2.Location = new System.Drawing.Point(650, 43);
+                this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+                this.label2.Name = "label2";
+                this.label2.Size = new System.Drawing.Size(131, 32);
+                this.label2.TabIndex = 0;
+                this.label2.Text = "عيادة الشفاء";
+                // 
+                // main
+                // 
+                this.main.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+                this.main.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.main.Location = new System.Drawing.Point(1035, 134);
+                this.main.Name = "main";
+                this.main.Size = new System.Drawing.Size(126, 43);
+                this.main.TabIndex = 6;
+                this.main.Text = "الصفحة الرئيسية";
+                this.main.UseVisualStyleBackColor = true;
+                this.main.Click += new System.EventHandler(this.main_Click);
+                // 
+                // connect
+                // 
+                this.connect.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+                this.connect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.connect.Location = new System.Drawing.Point(255, 134);
+                this.connect.Name = "connect";
+                this.connect.Size = new System.Drawing.Size(126, 43);
+                this.connect.TabIndex = 5;
+                this.connect.Text = "تواصل معنا";
+                this.connect.UseVisualStyleBackColor = true;
+                this.connect.Click += new System.EventHandler(this.connect_Click);
+                // 
+                // reserve
+                // 
+                this.reserve.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+                this.reserve.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.reserve.Location = new System.Drawing.Point(456, 134);
+                this.reserve.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.reserve.Name = "reserve";
+                this.reserve.Size = new System.Drawing.Size(126, 43);
+                this.reserve.TabIndex = 4;
+                this.reserve.Text = "لحجز موعد";
+                this.reserve.UseVisualStyleBackColor = true;
+                this.reserve.Click += new System.EventHandler(this.reserve_Click);
+                // 
+                // commant
+                // 
+                this.commant.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+                this.commant.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.commant.Location = new System.Drawing.Point(655, 134);
+                this.commant.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.commant.Name = "commant";
+                this.commant.Size = new System.Drawing.Size(126, 43);
+                this.commant.TabIndex = 3;
+                this.commant.Text = "التعليقات";
+                this.commant.UseVisualStyleBackColor = true;
+                this.commant.Click += new System.EventHandler(this.commant_Click);
+                // 
+                // about
+                // 
+                this.about.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+                this.about.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.about.Location = new System.Drawing.Point(836, 134);
+                this.about.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.about.Name = "about";
+                this.about.Size = new System.Drawing.Size(126, 43);
+                this.about.TabIndex = 2;
+                this.about.Text = "عن الموقع";
+                this.about.UseVisualStyleBackColor = true;
+                this.about.Click += new System.EventHandler(this.about_Click);
+                // 
+                // button1
+                // 
+                this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.button1.Location = new System.Drawing.Point(1656, 148);
+                this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.button1.Name = "button1";
+                this.button1.Size = new System.Drawing.Size(180, 75);
+                this.button1.TabIndex = 1;
+                this.button1.Text = "الصفحة الرئيسية";
+                this.button1.UseVisualStyleBackColor = true;
+                // 
+                // label9
+                // 
+                this.label9.AutoSize = true;
+                this.label9.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold);
+                this.label9.Location = new System.Drawing.Point(654, 15);
+                this.label9.Name = "label9";
+                this.label9.Size = new System.Drawing.Size(83, 19);
+                this.label9.TabIndex = 16;
+                this.label9.Text = ":اختر الطبيبة";
+                // 
+                // label1
+                // 
+                this.label1.AutoSize = true;
+                this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+                this.label1.Location = new System.Drawing.Point(650, 43);
+                this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+                this.label1.Name = "label1";
+                this.label1.Size = new System.Drawing.Size(131, 32);
+                this.label1.TabIndex = 0;
+                this.label1.Text = "عيادة الشفاء";
+                // 
+                // label4
+                // 
+                this.label4.AutoSize = true;
+                this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+                this.label4.Location = new System.Drawing.Point(582, 8);
+                this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+                this.label4.Name = "label4";
+                this.label4.Size = new System.Drawing.Size(229, 32);
+                this.label4.TabIndex = 10;
+                this.label4.Text = "اكتب تعليقك عن الطبيبة";
+                // 
+                // panel4
+                // 
+                this.panel4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+                this.panel4.Controls.Add(this.sendComment);
+                this.panel4.Controls.Add(this.textBox1);
+                this.panel4.Controls.Add(this.label5);
+                this.panel4.Controls.Add(this.comboBox1);
+                this.panel4.Controls.Add(this.label9);
+                this.panel4.Cursor = System.Windows.Forms.Cursors.Arrow;
+                this.panel4.Location = new System.Drawing.Point(318, 332);
+                this.panel4.Name = "panel4";
+                this.panel4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+                this.panel4.Size = new System.Drawing.Size(766, 362);
+                this.panel4.TabIndex = 17;
+                this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+                // 
+                // textBox1
+                // 
+                this.textBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.textBox1.ForeColor = System.Drawing.Color.Gray;
+                this.textBox1.Location = new System.Drawing.Point(102, 134);
+                this.textBox1.Multiline = true;
+                this.textBox1.Name = "textBox1";
+                this.textBox1.Size = new System.Drawing.Size(632, 99);
+                this.textBox1.TabIndex = 20;
+                this.textBox1.Text = "اكتب تعليقك ...";
+                this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+                this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+                // 
+                // label5
+                // 
+                this.label5.AutoSize = true;
+                this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold);
+                this.label5.Location = new System.Drawing.Point(687, 103);
+                this.label5.Name = "label5";
+                this.label5.Size = new System.Drawing.Size(50, 19);
+                this.label5.TabIndex = 19;
+                this.label5.Text = ":تعليقك";
+                // 
+                // comboBox1
+                // 
+                this.comboBox1.FormattingEnabled = true;
+                this.comboBox1.Items.AddRange(new object[] {
+            "د. سماء - قلب",
+            "د. رياء - أسنان",
+            "د. سندس - أطفال"});
+                this.comboBox1.Location = new System.Drawing.Point(616, 48);
+                this.comboBox1.Name = "comboBox1";
+                this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+                this.comboBox1.Size = new System.Drawing.Size(121, 24);
+                this.comboBox1.TabIndex = 18;
+                // 
+                // panel3
+                // 
+                this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.panel3.Controls.Add(this.label4);
+                this.panel3.Location = new System.Drawing.Point(3, 240);
+                this.panel3.Name = "panel3";
+                this.panel3.Size = new System.Drawing.Size(1345, 62);
+                this.panel3.TabIndex = 16;
+                // 
+                // panel1
+                // 
+                this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+                this.panel1.Controls.Add(this.panel2);
+                this.panel1.Controls.Add(this.main);
+                this.panel1.Controls.Add(this.connect);
+                this.panel1.Controls.Add(this.reserve);
+                this.panel1.Controls.Add(this.commant);
+                this.panel1.Controls.Add(this.about);
+                this.panel1.Controls.Add(this.button1);
+                this.panel1.Controls.Add(this.label1);
+                this.panel1.Location = new System.Drawing.Point(3, 1);
+                this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.panel1.Name = "panel1";
+                this.panel1.Size = new System.Drawing.Size(1345, 202);
+                this.panel1.TabIndex = 14;
+                // 
+                // Form4
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(1349, 734);
+                this.Controls.Add(this.panel4);
+                this.Controls.Add(this.panel3);
+                this.Controls.Add(this.panel1);
+                this.Name = "Form4";
+                this.Text = "Form4";
+                this.Load += new System.EventHandler(this.Form4_Load);
+
+                this.panel2.ResumeLayout(false);
+                this.panel2.PerformLayout();
+                this.panel4.ResumeLayout(false);
+                this.panel4.PerformLayout();
+                this.panel3.ResumeLayout(false);
+                this.panel3.PerformLayout();
+                this.panel1.ResumeLayout(false);
+                this.panel1.PerformLayout();
+                this.ResumeLayout(false);
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.Button sendComment;
+            private System.Windows.Forms.Panel panel2;
+            private System.Windows.Forms.Button button12;
+            private System.Windows.Forms.Label label2;
+            private System.Windows.Forms.Button main;
+            private System.Windows.Forms.Button connect;
+            private System.Windows.Forms.Button reserve;
+            private System.Windows.Forms.Button commant;
+            private System.Windows.Forms.Button about;
+            private System.Windows.Forms.Button button1;
+            private System.Windows.Forms.Label label9;
+            private System.Windows.Forms.Label label1;
+            private System.Windows.Forms.Label label4;
+            private System.Windows.Forms.Panel panel4;
+            private System.Windows.Forms.Panel panel3;
+            private System.Windows.Forms.Panel panel1;
+            private System.Windows.Forms.ComboBox comboBox1;
+            private System.Windows.Forms.TextBox textBox1;
+            private System.Windows.Forms.Label label5;
+        }
+    }
